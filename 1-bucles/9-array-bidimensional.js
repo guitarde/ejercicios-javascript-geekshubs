8954bdd315cb8fe0d918b8e468e68713f1fa41ff
Luis Reyna <luisantoinfo@gmail.com>
2020-06-18 14:45:43 +0200

    Finalizada la unidad 1 de bucles javacript

--- 1-bucles/9-array-bidimensional.js
+++ 1-bucles/9-array-bidimensional.js
@@ -1,4 +1,3 @@
-
 // ej 9
 // Rellena la matriz como la siguiente utilizando bucles for anidados
 // let matriz = [
@@ -12,3 +11,11 @@ let matriz = [
     [],
     []
 ]
+
+
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        matriz[i][j] = i + '' + j;
+    }
+}
+console.log(matriz);
